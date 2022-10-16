7a1e2d7bc7996c7c82caa3492de734c2b7fef290
AnitaDS22 <112985720+AnitaDS22@users.noreply.github.com>
2022-10-16 19:43:14 -0300

    ultimos

--- signo.js
+++ signo.js
@@ -3,20 +3,19 @@
 let nombreIngresado = prompt ("ingrese su nombre");
 let edad = promt ("ingrese su edad");
 let pais = prompt ("ingrese su pais");
+let notaMate = prompt ("ingrese nota de matematica");
+let notaLengua = prompt ("ingrese nota de Lengua");
 
-let anioNacimiento = 2022 -  parseInt (edad);
+let notamatematica = parseInt (notaMate);
+let notafinallengua = parseInt (notaLengua);
+
+notaPromedio = (notafinallengua + notamatematica) /2; 
+
+alert ("HOLA " + nombreIngresado + " tu promedio es " + notaPromedio);
+
+
+
 
 alert ("hola, tu nombre es " + nombreIngresado + " tu edad es " + edad + "sos de " + pais + "naciste en el año " + anioNacimiento );
 
-
-
- /*
-console.log  ("mensaje");
-
-let nombre = ("Kiara y Yago");
-
-console.log (nombre);
-
-alert ("hola");
-*/
 
