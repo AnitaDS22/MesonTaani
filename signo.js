6a19ff315112145e3f642b9b2f34e86878716143
AnitaDS22 <112985720+AnitaDS22@users.noreply.github.com>
2022-10-16 16:06:57 -0300

    cambios

---
+++ signo.js
@@ -0,0 +1,22 @@
+
+
+let nombreIngresado = prompt ("ingrese su nombre");
+let edad = promt ("ingrese su edad");
+let pais = prompt ("ingrese su pais");
+
+let anioNacimiento = 2022 -  parseInt (edad);
+
+alert ("hola, tu nombre es " + nombreIngresado + " tu edad es " + edad + "sos de " + pais + "naciste en el año " + anioNacimiento );
+
+
+
+ /*
+console.log  ("mensaje");
+
+let nombre = ("Kiara y Yago");
+
+console.log (nombre);
+
+alert ("hola");
+*/
+
